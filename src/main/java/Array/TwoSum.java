9d4a574b7201f7cb928d71d9d1e697bb0b491162
Your Name <chewyeedhong@gmail.com>
2023-01-26 16:19:36 +0800

    LeetCode TwoSum Problem

---
+++ src/main/java/Array/TwoSum.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main (String [] args) {
+        int [] nums = new int[]{3,2,4};
+        int target = 6;
+        int [] output = new int[2];
+
+        Map<Integer, Integer> components = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int ans = target - nums[i];
+            if(!components.containsKey(nums[i]))
+                components.put(ans, i);
+            else
+                System.out.print(i+ " "+ components.get(nums[i]));
+        }
+
+    }
+
+
+}
